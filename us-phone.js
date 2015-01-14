f0cf54a35e1a841b6963ef1f3a380dc9baf4b45f
Tim <murano1980@gmail.com>
2015-01-15 00:02:09 +0300

    Update us-phone.js

--- us-phone.js
+++ us-phone.js
@@ -1,17 +1,22 @@
 angular.module('usPhone', []);
 angular.module('usPhone').filter('phonenumber', function() {
-    return function (number) {       
+    return function (number) {
         if (!number) { return ''; }
         number = String(number);
         var formattedNumber = number;
-        var c = (number[0] == '1') ? '1 ' : '';
-        number = number[0] == '1' ? number.slice(1) : number;
+        var c = (number[0] == '+7') ? '+7 ' : '';
+        number = number[0] == '+7' ? number.slice(2) : number;
         var area = number.substring(0,3);
         var front = number.substring(3, 6);
-        var end = number.substring(6, 10);
+        var middle = number.substring(6, 8);
+        var end = number.substring(8, 10);
         if (front) {
             formattedNumber = (c + "(" + area + ") " + front);
         }
+        if (middle) {
+            formattedNumber += ("-" + middle);
+        }
+
         if (end) {
             formattedNumber += ("-" + end);
         }
@@ -20,16 +25,16 @@ angular.module('usPhone').filter('phonenumber', function() {
 });
 
 angular.module('usPhone').directive('usPhoneDirective', ['$filter', function($filter){
-	return {
-		restrict: 'A',
-		scope: {
-        	ngModel: '='
-    	},
-		link: function($scope, iElm, iAttrs, controller) {
-			$scope.$watch('ngModel', function(value, oldValue) {
-	            var number = String(value).replace(/[^0-9]+/g, '');
-	            $scope.ngModel = $filter('phonenumber')(number);
-	        });
-		}
-	};
+    return {
+        restrict: 'A',
+        scope: {
+            ngModel: '='
+        },
+        link: function($scope, iElm, iAttrs, controller) {
+            $scope.$watch('ngModel', function(value, oldValue) {
+                var number = String(value).replace(/[^0-9]+/g, '');
+                $scope.ngModel = $filter('phonenumber')(number);
+            });
+        }
+    };
 }]);
